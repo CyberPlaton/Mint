e5491e259e09f69478fe13e4182d5187e4b02278
Bogdan Strohonov <bogdan2006@web.de>
2023-05-14 22:08:32 +0200

    Fixed: Transform editor.

--- Mint/Mint/src/Editor/ComponentEditor/ComponentEditors/TransformComponentEditor.cpp
+++ Mint/Mint/src/Editor/ComponentEditor/ComponentEditors/TransformComponentEditor.cpp
@@ -39,32 +39,23 @@ namespace mint::editor
 		ImGuiID slid = 10000;
 		ImGuiID scid = 20000;
 
-
-		auto position = CUCA::transform_get_position(entity);
-		auto scale = CUCA::transform_get_scale(entity);
-		auto rotation = CUCA::transform_get_rotation(entity);
+		auto& transform = MINT_ACTIVE_SCENE()->get_registry().get_component< mint::component::STransform >(entity);
 
 		
-		CUI::edit_field_vec2(position, gd.s_EditorTransformMinPosition, gd.s_EditorTransformMaxPosition,
+		CUI::edit_field_vec2(transform.m_position, gd.s_EditorTransformMinPosition, gd.s_EditorTransformMaxPosition,
 							 "Position", "Entities current position in the world, this affects all children positions recursively", slid++, scid++);
 		
 		ImGui::Separator();
 
 
-		CUI::edit_field_vec2(scale, gd.s_EditorTransformMinScale, gd.s_EditorTransformMaxScale,
+		CUI::edit_field_vec2(transform.m_scale, gd.s_EditorTransformMinScale, gd.s_EditorTransformMaxScale,
 							 "Scale", "Entities current scale, this affects all children scale values recursively", slid++, scid++, ImGuiSliderFlags_Logarithmic);
 
 		ImGui::Separator();
 
 
-		CUI::edit_field_f32(rotation, gd.s_EditorTransformMinRotation, gd.s_EditorTransformMaxRotation,
+		CUI::edit_field_f32(transform.m_rotation, gd.s_EditorTransformMinRotation, gd.s_EditorTransformMaxRotation,
 							"Rotation", "Entities current rotation, this affects all children rotation values recursively", slid++, scid++, ImGuiSliderFlags_Logarithmic);
-
-
-
-		CUCA::transform_set_position(entity, position);
-		CUCA::transform_set_scale(entity, scale);
- 		CUCA::transform_set_rotation(entity, rotation);
 	}
 
 
