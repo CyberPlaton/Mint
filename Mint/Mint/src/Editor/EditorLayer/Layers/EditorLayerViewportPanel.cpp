58755ba97ebdfa84a9ce84100147fb70b21c04a0
Bogdan Strohonov <bogdan2006@web.de>
2023-05-22 00:38:40 +0200

    Fix: do not render sprite origin if entity does not have a sprite. do not render destination rectangle if entity does not have a transform.

--- Mint/Mint/src/Editor/EditorLayer/Layers/EditorLayerViewportPanel.cpp
+++ Mint/Mint/src/Editor/EditorLayer/Layers/EditorLayerViewportPanel.cpp
@@ -92,9 +92,15 @@ namespace mint::editor
 				{
 					Vector< entt::entity > entities{ GlobalData::Get().s_EditorInspectedEntity };
 
-					if (m_renderDestinationRect) fx::CDebugRenderer::on_render_destination_rectangle(entities);
+					if(m_renderDestinationRect && MINT_ACTIVE_SCENE()->get_registry().has_component< mint::component::STransform >(GlobalData::Get().s_EditorInspectedEntity))
+					{
+						fx::CDebugRenderer::on_render_destination_rectangle(entities);
+					}
 
-					if (m_renderOriginPoint) fx::CDebugRenderer::on_render_sprite_origin(entities);
+					if (m_renderOriginPoint && MINT_ACTIVE_SCENE()->get_registry().has_component< mint::component::SSprite >(GlobalData::Get().s_EditorInspectedEntity))
+					{
+						fx::CDebugRenderer::on_render_sprite_origin(entities);
+					}
 				}
 			}
 
