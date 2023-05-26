e031e9994005c821d76273e90f011dee90eafea3
bstrohonov <bogdan2006@web.de>
2023-05-27 00:15:16 +0200

    Refactor: displaying blending factors and equation.

--- Mint/Mint/src/Editor/MaterialEditor/MaterialEditor.cpp
+++ Mint/Mint/src/Editor/MaterialEditor/MaterialEditor.cpp
@@ -60,11 +60,172 @@ namespace mint::editor
 
 	void CMaterialEditor::main_frame()
 	{
-			
+		ImGuiID sid = 10000;
+		ImGuiID scid = 20000;
+
+		CUI::edit_field_string(m_materialDefinition.m_materialName, "Material Name", "", sid++, scid++);
+		CUI::edit_field_string(m_materialDefinition.m_textureName, "Texture Name", "", sid++, scid++);
+		CUI::edit_field_string(m_materialDefinition.m_shaderProgramName, "Shader Program Name", "", sid++, scid++);
+
+		ImGui::SeparatorText("Blending");
+
+		int current_blending_mode = (int)m_materialDefinition.m_blendMode;
+		if (ImGui::Combo("Mode", &current_blending_mode, s_BlendingModeOptions, IM_ARRAYSIZE(s_BlendingModeOptions)))
+		{
+			m_materialDefinition.m_blendMode = (BlendMode)current_blending_mode;
+		}
+
+		int current_blending_src = get_blending_src_factor_option_number(m_materialDefinition.m_srcBlendFactor);
+		if (ImGui::Combo("Source Factor", &current_blending_src, s_BlendingFactorOptions, IM_ARRAYSIZE(s_BlendingFactorOptions)))
+		{
+			set_blending_src_factor(current_blending_src);
+		}
+
+		int current_blending_dst = get_blending_dst_factor_option_number(m_materialDefinition.m_dstBlendFactor);
+		if (ImGui::Combo("Destination Factor", &current_blending_dst, s_BlendingFactorOptions, IM_ARRAYSIZE(s_BlendingFactorOptions)))
+		{
+			set_blending_dst_factor(current_blending_dst);
+		}
+
+		int current_blending_equat = get_blending_equation_option_number(m_materialDefinition.m_blendingEquation);
+		if (ImGui::Combo("Equation", &current_blending_equat, s_BlendingEquationOptions, IM_ARRAYSIZE(s_BlendingEquationOptions)))
+		{
+			set_blending_equation(current_blending_equat);
+		}
+
+
 	}
 
 	void CMaterialEditor::on_update(f32 dt)
 	{
+	}
+
+	void CMaterialEditor::set_blending_src_factor(int option_number)
+	{
+		switch (option_number)
+		{
+		case 0: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_Zero; break;
+		case 1: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_One; break;
+		case 2: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_SrcColor; break;
+		case 3: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_OneMinusSrcColor; break;
+		case 4: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_SrcAlpha; break;
+		case 5: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_OneMinusSrcAlpha; break;
+		case 6: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_DstAlpha; break;
+		case 7: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_DstColor; break;
+		case 8: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_OneMinusDstAlpha; break;
+		case 9: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_SrcAlphaSaturate; break;
+		case 10: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_ConstantColor; break;
+		case 11: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_OneMinusConstantColor; break;
+		case 12: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_ConstantAlpha; break;
+		case 13: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_OneMinusConstantAlpha; break;
+		default: m_materialDefinition.m_srcBlendFactor = mint::fx::BlendingFactor_Zero; break;
+		}
+	}
+
+	void CMaterialEditor::set_blending_dst_factor(int option_number)
+	{
+		switch (option_number)
+		{
+		case 0: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_Zero; break;
+		case 1: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_One; break;
+		case 2: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_SrcColor; break;
+		case 3: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_OneMinusSrcColor; break;
+		case 4: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_SrcAlpha; break;
+		case 5: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_OneMinusSrcAlpha; break;
+		case 6: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_DstAlpha; break;
+		case 7: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_DstColor; break;
+		case 8: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_OneMinusDstAlpha; break;
+		case 9: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_SrcAlphaSaturate; break;
+		case 10: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_ConstantColor; break;
+		case 11: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_OneMinusConstantColor; break;
+		case 12: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_ConstantAlpha; break;
+		case 13: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_OneMinusConstantAlpha; break;
+		default: m_materialDefinition.m_dstBlendFactor = mint::fx::BlendingFactor_Zero; break;
+		}
+	}
+
+	void CMaterialEditor::set_blending_equation(int option_number)
+	{
+		switch (option_number)
+		{
+		case 0: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_Add; break;
+		case 1: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_Min; break;
+		case 2: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_Max; break;
+		case 3: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_Subtract; break;
+		case 4: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_ReverseSubtract; break;
+		case 5: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_BlendEquationRGB; break;
+		case 6: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_BlendEquationAlpha; break;
+		case 7: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_BlendDstRGB; break;
+		case 8: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_BlendSrcRGB; break;
+		case 9: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_BlendDstAlpha; break;
+		case 10: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_BlendSrcAlpha; break;
+		case 11: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_BlendColor; break;
+		default: m_materialDefinition.m_blendingEquation = mint::fx::BlendingEquation_BlendColor; break;
+		}
+	}
+
+	int CMaterialEditor::get_blending_src_factor_option_number(uint32_t blending_factor)
+	{
+		switch ((mint::fx::BlendingFactor)blending_factor)
+		{
+		case mint::fx::BlendingFactor_Zero:						return 0;
+		case mint::fx::BlendingFactor_One:						return 1;
+		case mint::fx::BlendingFactor_SrcColor:					return 2;
+		case mint::fx::BlendingFactor_OneMinusSrcColor:			return 3;
+		case mint::fx::BlendingFactor_SrcAlpha:					return 4;
+		case mint::fx::BlendingFactor_OneMinusSrcAlpha:			return 5;
+		case mint::fx::BlendingFactor_DstAlpha:					return 6;
+		case mint::fx::BlendingFactor_DstColor:					return 7;
+		case mint::fx::BlendingFactor_OneMinusDstAlpha:			return 8;
+		case mint::fx::BlendingFactor_SrcAlphaSaturate:			return 9;
+		case  mint::fx::BlendingFactor_ConstantColor:			return 10;
+		case  mint::fx::BlendingFactor_OneMinusConstantColor:	return 11;
+		case  mint::fx::BlendingFactor_ConstantAlpha:			return 12;
+		case  mint::fx::BlendingFactor_OneMinusConstantAlpha:	return 13;
+		default: return 0;
+		}
+	}
+
+	int CMaterialEditor::get_blending_dst_factor_option_number(uint32_t blending_factor)
+	{
+		switch ((mint::fx::BlendingFactor)blending_factor)
+		{
+		case mint::fx::BlendingFactor_Zero:						return 0;
+		case mint::fx::BlendingFactor_One:						return 1;
+		case mint::fx::BlendingFactor_SrcColor:					return 2;
+		case mint::fx::BlendingFactor_OneMinusSrcColor:			return 3;
+		case mint::fx::BlendingFactor_SrcAlpha:					return 4;
+		case mint::fx::BlendingFactor_OneMinusSrcAlpha:			return 5;
+		case mint::fx::BlendingFactor_DstAlpha:					return 6;
+		case mint::fx::BlendingFactor_DstColor:					return 7;
+		case mint::fx::BlendingFactor_OneMinusDstAlpha:			return 8;
+		case mint::fx::BlendingFactor_SrcAlphaSaturate:			return 9;
+		case  mint::fx::BlendingFactor_ConstantColor:			return 10;
+		case  mint::fx::BlendingFactor_OneMinusConstantColor:	return 11;
+		case  mint::fx::BlendingFactor_ConstantAlpha:			return 12;
+		case  mint::fx::BlendingFactor_OneMinusConstantAlpha:	return 13;
+		default: return 0;
+		}
+	}
+
+	int CMaterialEditor::get_blending_equation_option_number(uint32_t blending_equation)
+	{
+		switch ((mint::fx::BlendingEquation)blending_equation)
+		{
+		case mint::fx::BlendingEquation_Add:				return 0;
+		case mint::fx::BlendingEquation_Min:				return 1;
+		case mint::fx::BlendingEquation_Max:				return 2;
+		case mint::fx::BlendingEquation_Subtract:			return 3;
+		case mint::fx::BlendingEquation_ReverseSubtract:	return 4;
+		case mint::fx::BlendingEquation_BlendEquationRGB:	return 5;
+		case mint::fx::BlendingEquation_BlendEquationAlpha:	return 6;
+		case mint::fx::BlendingEquation_BlendDstRGB:		return 7;
+		case mint::fx::BlendingEquation_BlendSrcRGB:		return 8;
+		case mint::fx::BlendingEquation_BlendDstAlpha:		return 9;
+		case  mint::fx::BlendingEquation_BlendSrcAlpha:		return 10;
+		case  mint::fx::BlendingEquation_BlendColor:		return 11;
+		default: return 0;
+		}
 	}
 
 }
--- Mint/Mint/src/Editor/MaterialEditor/MaterialEditor.h
+++ Mint/Mint/src/Editor/MaterialEditor/MaterialEditor.h
@@ -11,6 +11,47 @@
 
 namespace mint::editor
 {
+	static const char* s_BlendingModeOptions[] = {
+		STRING(BLEND_ALPHA),
+		STRING(BLEND_ADDITIVE),
+		STRING(BLEND_MULTIPLIED),
+		STRING(BLEND_ADD_COLORS),
+		STRING(BLEND_SUBTRACT_COLORS),
+		STRING(BLEND_ALPHA_PREMULTIPLY)
+	};
+
+	static const char* s_BlendingFactorOptions[] = {
+		STRING(BlendingFactor_Zero),
+		STRING(BlendingFactor_One),
+		STRING(BlendingFactor_SrcColor),
+		STRING(BlendingFactor_OneMinusSrcColor),
+		STRING(BlendingFactor_SrcAlpha),
+		STRING(BlendingFactor_OneMinusSrcAlpha),
+		STRING(BlendingFactor_DstAlpha),
+		STRING(BlendingFactor_DstColor),
+		STRING(BlendingFactor_OneMinusDstAlpha),
+		STRING(BlendingFactor_SrcAlphaSaturate),
+		STRING(BlendingFactor_ConstantColor),
+		STRING(BlendingFactor_OneMinusConstantColor),
+		STRING(BlendingFactor_ConstantAlpha),
+		STRING(BlendingFactor_OneMinusConstantAlpha),
+	};
+
+	static const char* s_BlendingEquationOptions[] = {
+		STRING(BlendingEquation_Add),
+		STRING(BlendingEquation_Min),
+		STRING(BlendingEquation_Max),
+		STRING(BlendingEquation_Subtract),
+		STRING(BlendingEquation_ReverseSubtract),
+		STRING(BlendingEquation_BlendEquationRGB),
+		STRING(BlendingEquation_BlendEquationAlpha),
+		STRING(BlendingEquation_BlendDstRGB),
+		STRING(BlendingEquation_BlendSrcRGB),
+		STRING(BlendingEquation_BlendDstAlpha),
+		STRING(BlendingEquation_BlendSrcAlpha),
+		STRING(BlendingEquation_BlendColor)
+	};
+
 
 	class CMaterialEditor
 	{
@@ -42,6 +83,18 @@ namespace mint::editor
 
 	private:
 		void main_frame();
+
+		void set_blending_src_factor(int option_number);
+
+		void set_blending_dst_factor(int option_number);
+
+		void set_blending_equation(int option_number);
+
+		int get_blending_src_factor_option_number(uint32_t blending_factor);
+
+		int get_blending_dst_factor_option_number(uint32_t blending_factor);
+
+		int get_blending_equation_option_number(uint32_t blending_equation);
 
 	};
 }
